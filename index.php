ee7494ea1dae23d0fabc897b8b2e1181d9f234c4
Benjamin Ducoin <megabigben44@gmail.com>
2020-07-20 16:03:05 +0200

    refonte

--- index.php
+++ index.php
@@ -1,46 +1,47 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Ceci est une page de test avec des balises PHP</title>
-        <meta charset="utf-8" />
-    </head>
-    <body>
-       <?php
-        include 'header.php'
-      ?>
-
-
-
-        <h2>Page de test</h2>
-
-        <p>
-            Cette page contient du code HTML avec des balises PHP.<br />
-            <?php /* Insérer du code PHP ici */ ?>
-            Voici quelques petits tests :
-        </p>
-
-        <ul>
-        <li style="color: blue;">Texte en bleu</li>
-        <li style="color: red;">Texte en rouge</li>
-        <li style="color: green;">Texte en vert</li>
-        </ul>
 
         <?php
-        include 'db.php';
+          session_start();
+
+          if (!isset($_SESSION['username'])) {
+          	$_SESSION['msg'] = "You must log in first";
+          	header('location: login.php');
+          }
+          if (isset($_GET['logout'])) {
+          	session_destroy();
+          	unset($_SESSION['username']);
+          	header("location: login.php");
+          }
         ?>
+        <!DOCTYPE html>
+        <html>
+        <head>
+        	<title>Home</title>
+        	<link rel="stylesheet" type="text/css" href="style.css">
+        </head>
+        <body>
 
+        <div class="header">
+        	<h2>Home Page</h2>
+        </div>
+        <div class="content">
+          	<!-- notification message -->
+          	<?php if (isset($_SESSION['success'])) : ?>
+              <div class="error success" >
+              	<h3>
+                  <?php
+                  	echo $_SESSION['success'];
+                  	unset($_SESSION['success']);
+                  ?>
+              	</h3>
+              </div>
+          	<?php endif ?>
 
+            <!-- logged in user information -->
+            <?php  if (isset($_SESSION['username'])) : ?>
+            	<p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+            	<p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
+            <?php endif ?>
+        </div>
 
-
-
-        <a href="inscription.php"> Inscription </a>
-        <p>
-        </p>
-        <a href="connexion.php"> connexion </a>
-
-        <?php
-        include 'footer.php'
-        ?>
-
-    </body>
-</html>
+        </body>
+        </html>
